b4df6e2fb88565d1e4d8df0bd4d5be0cf4465793
Tomer kalif <tomerkalif@Tomers-MacBook-Air.local>
2025-10-19 15:34:50 +0300

    continued work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world")
+import { red } from "./templates/redPanels/index.js";
+
+const redDashboard = red({
+  dashboardTitle: "RED method",
+  serviceIds: ["express-demo-server"],
+});
+
+const dashboard = redDashboard.build();
+
+// Copy to clipboard
+const dashboardJson = JSON.stringify(dashboard, null, 2);
+
+
+console.log(dashboardJson);
